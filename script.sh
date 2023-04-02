2c094f203c29d686dd836b24ae2ca0995c5009b4
Mastermod1 <danielkalmucki2@gmail.com>
2023-04-02 15:56:36 +0200

    Added file generator

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 if [[ "$1" == "--date" ]]; then
 	date
+elif [[ "$1" == "--logs" ]]; then
+	for ((i = 1 ; i < 101 ; i++)); do
+	  echo "$0" >> "log$i.txt"
+	  date >> "log$i.txt"
+	done
 fi

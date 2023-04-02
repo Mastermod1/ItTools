292d335a5fbeaf98ae5517a3f2df283405546472
Mastermod1 <danielkalmucki2@gmail.com>
2023-04-02 17:11:03 +0200

    Parametrized log creation

--- script.sh
+++ script.sh
@@ -2,8 +2,15 @@
 if [[ "$1" == "--date" ]]; then
 	date
 elif [[ "$1" == "--logs" ]]; then
-	for ((i = 1 ; i < 101 ; i++)); do
-	  echo "$0" >> "log$i.txt"
-	  date >> "log$i.txt"
+	declare -i logsNumber=100
+	if [[ -n "$2" ]]; then
+		logsNumber=$2
+	fi
+	i=1
+	while [ $i -le $logsNumber ]; 
+	do 
+		echo "$0" >> "log$i.txt"
+		date >> "log$i.txt"
+		i=$(($i+1))
 	done
 fi
